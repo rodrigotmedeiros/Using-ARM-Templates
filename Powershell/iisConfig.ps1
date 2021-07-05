7d5b11998a405432c040ea23debd433c1d836dbf
rodrigo.tancik <rodrigotancik@gmail.com>
2021-07-05 02:32:32 -0300

    Renomeado os arquivos

--- Powershell/iisConfig.ps1
+++ Powershell/iisConfig.ps1
@@ -10,12 +10,12 @@ $username = "$user@$domain"
 
 $password = ConvertTo-SecureString $pass -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username,$password
-$adJoin = $null
 
 do{
     Add-Computer -DomainName $domain -DomainCredential $Credential -ErrorAction 'silentlycontinue'
     if($?){
         $adJoinSucceded = $true
+        Break
     }else{
         $adJoinSucceded = $false
     }
--- Powershell/powershell.ps1
+++ Powershell/powershell.ps1
@@ -3,7 +3,7 @@
 
 # ** VARIÁVEIS **
 $rgName = "lab"
-$rgRegion = "CentralUS"
+$rgRegion = "CentralUs"
 # Coleta o IP público do usuário, a fim de liberar somente esse IP para acessar os recursos dentro da VNET, utilizando NSG
 $publicIpAddress = Invoke-WebRequest -Uri ifconfig.me -UseBasicParsing | Select-Object -ExpandProperty Content 
 $keyvaultPassSecret = "vmPass"
@@ -24,9 +24,9 @@ $adDomainName = "contoso.corp"
 $resourceDeploymentJsonFile = "ARM_Templates\resourcesDeploymentParameters.json"
 $addsConfigurationJsonFile = "ARM_Templates\addsConfigurationParameters.json"
 # Gera um nome único para o KeyVault (o nome deve ser único dentro do Azure)
-$nameLenght = 4
-$kvCaracteres = "qazwsedcrfvtgbyhnujmikopQAZWSEDCRFVTGBYHNUJMKLP123456789"
-$keyvaultName = "kvstorerdn" + -join ($kvCaracteres.ToCharArray() | Get-Random -Count $nameLenght)
+$keyvaultNameLenght = 4
+$keyvaultCaracteres = "qazwsedcrfvtgbyhnujmikopQAZWSEDCRFVTGBYHNUJMKLP123456789"
+$keyvaultName = "kvstorerdn" + -join ($keyvaultCaracteres.ToCharArray() | Get-Random -Count $keyvaultNameLenght)
 # Gera uma senha única para as VMs
 $passLenght = 22
 $passCaracteres = "qazwsedcrfvtgbyhnujmikopQAZWSEDCRFVTGBYHNUJMKLP123456789*$@"
@@ -34,10 +34,11 @@ $passCaracteres = "qazwsedcrfvtgbyhnujmikopQAZWSEDCRFVTGBYHNUJMKLP123456789*$@"
 # Cria um Resource Group
 Clear-Host
 Write-Host ""
-Write-Host "** CRIANDO O RESOURCE GROUP '"$rgName"'!"
+Write-Host "CRIANDO O RESOURCE GROUP `"$rgName`"..."
+Write-Host ""
 New-AzResourceGroup `
   -Name $rgName `
-  -Location $rgRegion
+  -Location $rgRegion | Out-Null
 
 # A variável '$?' do Powershell armazena um valor TRUE se o último comando executado foi bem sucedido (New-AzResourceGroup) e FALSE se não. 
 # Aqui, '!$?' significa 'SE NÃO FOR TRUE, PARE DE EXECUTAR'
@@ -46,43 +47,40 @@ if(!$?){
 }
 
 # Cria o KeyVault
-Clear-Host
-Write-Host "CRIADO COM SUCESSO O RESOURCE GROUP '"$rgName"'!"
-Write-Host "** CRIANDO O KEYVAULT '"$keyvaultName"'"
+Write-Host "CRIADO COM SUCESSO O RESOURCE GROUP `"$rgName`"!"
+Write-Host "CRIANDO O KEYVAULT `"$keyvaultName`"..."
 Write-Host ""
-$keyVaultInfo = New-AzKeyVault `
+New-AzKeyVault `
   -VaultName $keyvaultName `
   -ResourceGroupName $rgName `
-  -Location $rgRegion -EnabledForTemplateDeployment `
-  -Verbose
+  -Location $rgRegion -EnabledForTemplateDeployment | Out-Null
+  # Poderia usar o parâmetro -Verbose para mostrar o status do deploy, mas só o status que defini com Write-Host já basta.
 
 if(!$?){
   Break
 }
 
 # Cria um KeyVault Secret para a senha das VMs
-Clear-Host
-Write-Host "CRIADO COM SUCESSO O KEYVAULT '"$keyvaultName"'!"
-Write-Host "** CRIANDO O KEYVAULT SECRET '"$keyvaultPassSecret"'!"
+Write-Host "CRIADO COM SUCESSO O KEYVAULT `"$keyvaultName`"!"
+Write-Host "CRIANDO O KEYVAULT SECRET `"$keyvaultPassSecret`"..."
 Write-Host ""
 Set-AzKeyVaultSecret `
   -VaultName $keyvaultName `
   -Name $keyvaultPassSecret `
-  -SecretValue (ConvertTo-SecureString (-join ($passCaracteres.ToCharArray() | Get-Random -Count $passLenght)) -AsPlainText -Force) 
+  -SecretValue (ConvertTo-SecureString (-join ($passCaracteres.ToCharArray() | Get-Random -Count $passLenght)) -AsPlainText -Force) | Out-Null
 
 if(!$?){
   Break
 } 
 
 # Cria um KeyVault Secret para a senha de recuperação do ADDS
-Clear-Host
-Write-Host "CRIADO COM SUCESSO O KEYVAULT SECRET '"$keyvaultPassSecret"'!"
-Write-Host "** CRIANDO O KEYVAULT SECRET '"$keyvaultADDSRecoverySecret"'"
+Write-Host "CRIADO COM SUCESSO O KEYVAULT SECRET `"$keyvaultPassSecret`"!"
+Write-Host "CRIANDO O KEYVAULT SECRET `"$keyvaultADDSRecoverySecret`"..."
 Write-Host ""
 Set-AzKeyVaultSecret `
   -VaultName $keyvaultName `
   -Name $keyvaultADDSRecoverySecret `
-  -SecretValue (ConvertTo-SecureString (-join ($passCaracteres.ToCharArray() | Get-Random -Count $passLenght)) -AsPlainText -Force) 
+  -SecretValue (ConvertTo-SecureString (-join ($passCaracteres.ToCharArray() | Get-Random -Count $passLenght)) -AsPlainText -Force) | Out-Null
 
 if(!$?){
   Break
@@ -91,6 +89,7 @@ if(!$?){
 # Atualiza o arquivo de parâmetros 'addsConfigurationParameters.json'. Ele é o responsável por referenciar a chave no KeyVault na criação das VMs
 # Aqui, o arquivo JSON será atualizado com as informações do KeyVault (id e secretname)
 # ARQUIVO DE PARÂMETROS DA SENHA DAS VMS
+$keyVaultInfo = Get-AzKeyVault | Where-Object {$_.VaultName -eq $keyvaultName}
 $resourcesDeployParameters = Get-Content $resourceDeploymentJsonFile -raw | ConvertFrom-Json
 $resourcesDeployParameters.parameters.adminPassword.reference.keyVault.id = $keyVaultInfo.ResourceId
 $resourcesDeployParameters.parameters.adminPassword.reference.secretName = $keyvaultPassSecret
@@ -103,11 +102,10 @@ $addsConfigurationParameters.parameters.addsRecoveryPassword.reference.secretNam
 $addsConfigurationParameters | ConvertTo-Json -Depth 4  | Set-Content $addsConfigurationJsonFile
 
 # Faz o deploy dos recursos
-Clear-Host
-Write-Host "CRIADO COM SUCESSO O KEYVAULT SECRET '"$keyvaultADDSRecoverySecret"'!"
-Write-Host "** REALIZANDO O DEPLOY DOS RECURSOS"
+Write-Host "CRIADO COM SUCESSO O KEYVAULT SECRET `"$keyvaultADDSRecoverySecret`"'!"
+Write-Host "REALIZANDO O DEPLOY DOS RECURSOS..."
 Write-Host ""
-$resourcesDeploymentFile = "ARM_Templates\resources-deploy.json"
+$resourcesDeploymentFile = "ARM_Templates\resourcesDeploy.json"
 $resourcesDeploymentName = "resources.deploy"
 $resourcesDeployment = New-AzResourceGroupDeployment `
   -Name $resourcesDeploymentName `
@@ -125,8 +123,7 @@ $resourcesDeployment = New-AzResourceGroupDeployment `
   -vmClientName $vmClientName `
   -vmClientSize $vmClientSize `
   -nsgName $nsgName `
-  -TemplateParameterFile $resourceDeploymentJsonFile `
-  -Verbose
+  -TemplateParameterFile $resourceDeploymentJsonFile
 
 if(!$?){
   Break
@@ -136,11 +133,10 @@ if(!$?){
 # Esse output vem do ARM Template executado anteriormente
 $vmADPrivateIp = $resourcesDeployment.Outputs.Item("vmADPrivateIp").value
 
-Clear-Host
 Write-Host "DEPLOY DOS RECURSOS REALIZADO COM SUCESSO!"
-Write-Host "** DEFININDO O IP PRIVADO DA VM ad-vm PARA STATIC"
+Write-Host "DEFININDO O IP PRIVADO DA VM `"ad-vm`" PARA STATIC..."
 Write-Host ""
-$vmAdUpdateDeploymentFile = "ARM_Templates\vm-ad-update.json"
+$vmAdUpdateDeploymentFile = "ARM_Templates\adIpUpdate.json"
 $vmAdUpdateDeploymentName = "vm-ad.update"
 New-AzResourceGroupDeployment `
   -Name $vmAdUpdateDeploymentName `
@@ -149,19 +145,17 @@ New-AzResourceGroupDeployment `
   -vmADPrivateIp $vmADPrivateIp `
   -vnetName $vnetName `
   -subnetServersName $subnetServersName `
-  -vmADName $vmADName `
-  -Verbose
+  -vmADName $vmADName | Out-Null
 
 if(!$?){
   Break
 } 
 
 # Instala a feature de ADDS na VM vm-ad
-Clear-Host
 Write-Host "SETADO O IP DA VM vm-ad PARA STATIC COM SUCESSO!"
-Write-Host "** INSTALANDO A FEATURE DO ADDS NA VM vm-ad"
+Write-Host "INSTALANDO A FEATURE DO ADDS NA VM `"vm-ad`"..."
 Write-Host ""
-$vmAdExtensionDeploymentFile = "ARM_Templates\vm-ad-extension.json"
+$vmAdExtensionDeploymentFile = "ARM_Templates\adExtension.json"
 $vmAdExtensionDeploymentName = "vm-ad.extension"
 New-AzResourceGroupDeployment `
   -Name $vmAdExtensionDeploymentName `
@@ -169,19 +163,17 @@ New-AzResourceGroupDeployment `
   -ResourceGroupName $rgName `
   -vmADName $vmADName `
   -adDomainName $adDomainName `
-  -TemplateParameterFile $addsConfigurationJsonFile `
-  -Verbose
+  -TemplateParameterFile $addsConfigurationJsonFile | Out-Null
 
 if(!$?){
   Break
 }  
 
 # Atualiza o DNS da VNET para utilizar o IP da vm-ad
-Clear-Host
 Write-Host "FEATURE DO ADDS INSTALADA COM SUCESSO!"
-Write-Host "** ATUALIZANDO O DNS DA VNET PARA UTILIZAR O IP PRIVADO DA vm-ad"
+Write-Host "ATUALIZANDO O DNS DA VNET PARA UTILIZAR O IP PRIVADO DA `"vm-ad`"..."
 Write-Host ""
-$vnetDnsUpdateDeploymentFile = "ARM_Templates\vnet-dns-update.json"
+$vnetDnsUpdateDeploymentFile = "ARM_Templates\dnsUpdate.json"
 $vnetDnsUpdateDeploymentName = "vnet.dns.update"
 New-AzResourceGroupDeployment `
   -Name $vnetDnsUpdateDeploymentName `
@@ -190,30 +182,27 @@ New-AzResourceGroupDeployment `
   -vmADPrivateIp $vmADPrivateIp `
   -vnetName $vnetName `
   -subnetServersName $subnetServersName `
-  -subnetClientName $subnetClientName `
-  -Verbose
+  -subnetClientName $subnetClientName | Out-Null
 
 if(!$?){
   Break
 }
 
 # Reinicia as VMs para que a alteração anterior entre em vigor
-Clear-Host
 Write-Host "DNS ATUALIZADO COM SUCESSO!"
-Write-Host "** REINICIANDO AS VMs PARA APLICAR A ATUALIZAÇÃO DE DNS"
+Write-Host "REINICIANDO AS VMs PARA APLICAR A ATUALIZAÇÃO DE DNS..."
 Write-Host ""
-Get-AzVM | Restart-AzVM
+Get-AzVM | Restart-AzVM | Out-Null
 
 if(!$?){
   Break
 } 
 
 # Instala a feature de IIS na vm-iis e ingressa ela ao domínio
-Clear-Host
-Write-Host "VMs REINICIADAS COM SUCESSO"
-Write-Host "** INSTALANDO A FEATURE DE IIS NA VM vm-iis E INGRESSANDO ELA NO DOMÍNIO"
+Write-Host "VMs REINICIADAS COM SUCESSO!"
+Write-Host "INSTALANDO A FEATURE DE IIS NA VM `"vm-iis`" E INGRESSANDO ELA NO DOMÍNIO..."
 Write-Host ""
-$vmIisExtensionDeploymentFile = "ARM_Templates\vm-iis-extension.json"
+$vmIisExtensionDeploymentFile = "ARM_Templates\iisExtension.json"
 $vmIisExtensionDeploymentName = "vm-iis.extension"
 New-AzResourceGroupDeployment `
   -Name $vmIisExtensionDeploymentName `
@@ -222,8 +211,7 @@ New-AzResourceGroupDeployment `
   -vmIISName $vmIISName `
   -adminUsername $adminUsername `
   -adDomainName $adDomainName `
-  -TemplateParameterFile $resourceDeploymentJsonFile `
-  -Verbose
+  -TemplateParameterFile $resourceDeploymentJsonFile | Out-Null
 
 if(!$?){
   Break
@@ -231,33 +219,39 @@ if(!$?){
 
 # Após completar, verificar se é possível acessar a página padrão do IIS usando o FQDN da vm-iis (vm-iis.recruitlab.local) Se sim, o lab funcionou
 
-Clear-Host
 Write-Host "TODOS OS PASSOS FORAM REALIZADOS COM SUCESSO!"
-$cleanAll = Read-Host "CASO QUEIRA DELETAR TODOS OS RECURSOS, PRESSIONE 1"
+Write-Host ""
+Write-Host "Agora, vamos ver se tudo ocorreu sem problemas, iremos acessar a `"vm-ad`":"
+Write-Host "Ao iniciar, no Server Manager, desative a opção `"IE Enhanced Security Configuration`;"
+Write-Host "Acesse pelo Internet Explorer o endereço vm-iis.$adDomainName. Se a tela padrão do IIS abrir, está tudo certo!"
+Write-Host "Aguarde, a janela do RDP iniciará em breve!"
+$vmADPIP = Get-AzPublicIpAddress | Where-Object {$_.Name -eq "vm-ad-pip"} | Select-Object -ExpandProperty IpAddress
+$vmPassword = Get-AzKeyVaultSecret -VaultName $keyvaultName -Name $keyvaultPassSecret -AsPlainText
+Start-Sleep -Seconds 10
 
-switch($cleanAll)
-{
-  1{
-    # Limpa todos os recursos, inclusive o Resource Group
-    Clear-Host
-    Write-Host "** DELETANDO TODOS OS RECURSOS"
-    $clearResourcesDeploymentFile = "ARM_Templates\rg-purge.json"
-    $clearResourcesDeploymentName = "resourcegroup.purge"
-    New-AzResourceGroupDeployment `
-      -Name $clearResourcesDeploymentName `
-      -TemplateUri $clearResourcesDeploymentFile `
-      -ResourceGroupName $rgName `
-      -Mode Complete `
-      -Force `
-      -Verbose
+# O comando cmdkey.exe cria, lista e exclui nomes de usuário e senhas ou credenciais armazenados.
+# /add: endereço do servidor /user:o usuário /pass: a senha
+cmdkey.exe /add:$vmADPIP /user:$adminUsername /pass:$vmPassword | Out-Null
+# Abre o RDP utilizando as configurações acima
+mstsc /v:$vmADPIP
 
-    Remove-AzResourceGroup -Name $rgName -Force
-  }
-}
+<# Limpa todos os recursos, inclusive o Resource Group
+Write-Host ""
+Write-Host "DELETANDO TODOS OS RECURSOS..."
+$clearResourcesDeploymentFile = "ARM_Templates\rg-purge.json"
+$clearResourcesDeploymentName = "resourcegroup.purge"
+New-AzResourceGroupDeployment `
+  -Name $clearResourcesDeploymentName `
+  -TemplateUri $clearResourcesDeploymentFile `
+  -ResourceGroupName $rgName `
+  -Mode Complete `
+  -Force  | Out-Null
+
+Remove-AzResourceGroup -Name $rgName -Force | Out-Null
 
 if(!$?){
   Break
 }
 
-Write-Host "OS RECURSOS FORAM DELETADOS COM SUCESSO"
-Get-AzResource | Where-Object {$_.Location -like $rgRegion}
+Write-Host ""
+Write-Host "OS RECURSOS FORAM DELETADOS COM SUCESSO!" #>
